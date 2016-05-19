d1f3473488279045e3c1ff3443c0b35d283f57cf
unknown <>
2016-05-19 15:55:38 +0800

    GmmFitting

---
+++ clustering/gmmFitting.m
@@ -0,0 +1,25 @@
+function [model,cluster_labels]=gmmFitting(X,para)
+%   input:
+%       para=[Nr of clusters, covariance matrix type]
+%       matrix type:
+%           1 for full diagonal matrix, 2 for full covariance matrix
+%   output: 
+%       model is the gmm model
+
+
+    switch para(2)
+        case 1
+            obj=gmdistribution.fit(X,para(1),'Start','randSample','CovType','diagonal');
+        case 2
+            obj=gmdistribution.fit(X,para(1),'Start','randSample','CovType','full');
+        otherwise
+            disp('Invalid covariance matrix type')
+    end
+    model=obj;
+    post_p=[];
+    for i=1:para(1)
+       gm=gmdistribution(obj.mu(i,:),obj.Sigma(:,:,i),1);
+       post_p=[post_p pdf(gm,X)*obj.PComponents(i)];
+    end
+    [Y,cluster_labels]=max(post_p,[],2);
+end
